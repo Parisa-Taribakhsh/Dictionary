017d12c7ab5636d4a36394cde8e52cbbeb05b845
parisa <parisa.taribakhsh@gmail.com>
2021-08-10 02:16:30 +0430

    implement modals and adding words to table

--- src/Components/AddWord.js
+++ src/Components/AddWord.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, TextField} from "@material-ui/core";
 
-export default function AddWord(){
+export default function AddWord(props) {
+    const [newWord, setNewWord] = useState('');
+    const [definition, setDefinition] = useState('');
 
-    return(
+    function addNewWord(e) {
+        e.preventDefault();
+        props.add(newWord, definition);
+        console.log(newWord, definition)
+    }
+
+    let inputWordHandler = e => setNewWord(e.target.value)
+
+    let inputDefinitionHandler = e => setDefinition(e.target.value)
+
+    return (
         <div className='container text-center p-5'>
             <div className='row'>
                 <div className='col-12 col-lg-5 my-2'>
@@ -13,6 +25,7 @@ export default function AddWord(){
                         multiline
                         variant="outlined"
                         className='w-100'
+                        onChange={inputWordHandler}
                     />
                 </div>
                 <div className='col-12 col-lg-5 my-2'>
@@ -22,15 +35,16 @@ export default function AddWord(){
                         multiline
                         variant="outlined"
                         className='w-100'
+                        onChange={inputDefinitionHandler}
                     />
                 </div>
                 <div className='col-12 col-lg-2 my-auto my-2'>
-                    <Button variant="contained" color="secondary" >
+                    <Button variant="contained" color="secondary" onClick={!!newWord && !!definition ? addNewWord : ''}>
                         ADD
                     </Button>
                 </div>
             </div>
-            </div>
+        </div>
     )
 
 }
